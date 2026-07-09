2ae67b04dc9ec4c8800308710cfbd57567242cac
Tshepo Khoza <tshepo@MacBookAir.home>
2026-07-09 11:50:09 +0200

    Allow production dashboard CORS

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:3001",
+        "https://fade-dashboard-qrdyk.sevalla.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
